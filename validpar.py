33f505ec76729d38f5fe15abc0ec20e98469524b
Furkan Topaloğlu <frkntplglu@gmail.com>
2020-08-17 00:47:11 +0300

    parantheses validation exercise was added

--- validpar.py
+++ validpar.py
@@ -1,6 +1,15 @@
-def validation(par):
-    # code will be here
+def validation(string):
+    stack = []
+    for i in string:
+        if(i == '('):
+            stack.append(i)
+        elif(i == ')'):
+            if(len(stack) == 0):
+                return False
+            else: 
+                stack.pop()
+    if(len(stack) == 0):
+        return True
+    return False
 
-
-print(validation(")"))
-
+print(validation(")(()))"))
